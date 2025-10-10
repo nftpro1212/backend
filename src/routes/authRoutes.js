2b81b92d81d0d34d78157f7b8b1a34e1b679d30b
nftpro1212 <fozilkhalimov95@gmail.com>
2025-10-10 06:21:02 +0500

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,7 @@
 import express from "express";
-import { handleTelegramLogin } from "../controllers/authController.js";
+import { handleTelegramLogin } from "../controllers/telegramController.js";
 
 const router = express.Router();
-
 router.post("/login", handleTelegramLogin);
 
 export default router;
