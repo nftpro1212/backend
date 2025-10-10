2ed4b0245b18c573d3cccf9ac92f995b9524c20b
nftpro1212 <fozilkhalimov95@gmail.com>
2025-10-10 06:59:24 +0500

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    telegramId: { type: String, required: true, unique: true },
+    telegramId: { type: Number, required: true, unique: true },
     username: { type: String },
-    firstName: { type: String },
-    lastName: { type: String },
-    photoUrl: { type: String },
-    isPremium: { type: Boolean, default: false },
-    referrals: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    first_name: { type: String },
+    last_name: { type: String },
+    avatar: { type: String },
+
+    referralCode: { type: String, unique: true },
+    referredBy: { type: String }, // referal orqali kirgan userni kodi
   },
   { timestamps: true }
 );
