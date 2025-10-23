d791bd08f28428b20740e37406500acf54063ad8
nftpro1212 <fozilkhalimov95@gmail.com>
2025-10-23 23:11:58 +0500

    Update Referral.js

--- src/models/Referral.js
+++ src/models/Referral.js
@@ -2,6 +2,16 @@ import mongoose from "mongoose";
 
 const referralSchema = new mongoose.Schema(
   {
+    referrerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    referredId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     referrerTgId: {
       type: String,
       required: true,
