e7c22423742b2cce0dfc283dfc49d30063bd911b
nftpro1212 <fozilkhalimov95@gmail.com>
2025-10-23 21:26:55 +0500

    Update Referral.js

--- src/models/Referral.js
+++ src/models/Referral.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const referralSchema = new mongoose.Schema(
   {
-    referrerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    referredId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    referrerTgId: { type: String, required: true }, // kim chaqirgan
+    referredTgId: { type: String, required: true }, // kim chaqirilgan
   },
   { timestamps: true }
 );
